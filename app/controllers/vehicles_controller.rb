dbb73c7949fdec46f8c551c909192a84321efbe7
Brian Donegan <brian.donegan@careerbuilder.com>
2015-11-09 20:05:06 -0500

    Added Features

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -69,6 +69,11 @@ class VehiclesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def vehicle_params
-      params.require(:vehicle).permit(:color, :condition, :price, :category_id, :mileage)
+      params.require(:vehicle).permit(:color, 
+                                      :condition, 
+                                      :price, 
+                                      :category_id, 
+                                      :mileage,
+                                      { feature_ids: [] })
     end
 end
